a991f345995df3f4522c6196dcd7a3ababb70f26
Michał Budzyński <budziq@gmail.com>
2012-08-13 23:27:09 +0200

    added unix line endigs and shebang line

--- src/opensubtitles.py
+++ src/opensubtitles.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import struct
 import sys
 import os
@@ -7,7 +9,7 @@ from xmlrpc.client import ServerProxy, Error
 
 class SubtitleDownload:
     '''Traverses a directory and all subdirectories and downloads subtitles.
-    
+
     Relies on an undocumented OpenSubtitles feature where subtitles seems to be
     sorted by filehash and popularity.
     '''
@@ -43,34 +45,34 @@ class SubtitleDownload:
         try:
             print("Login...")
             self.login()
-            
+
             print("Searching for subtitles...")
             self.search_subtitles()
-            
+
             print("Logout...")
             self.logout()
         except Error as e:
             print("XML-RPC error:", e)
         except UserWarning as uw:
             print(uw)
-    
+
     def login(self):
         '''Log in to OpenSubtitles'''
         resp = self.server.LogIn('', '', 'en', 'OS Test User Agent')
         self.check_status(resp)
         self.login_token = resp['token']
-    
+
     def logout(self):
         '''Log out from OpenSubtitles'''
         resp = self.server.LogOut(self.login_token)
         self.check_status(resp)
-    
+
     def search_subtitles(self):
         '''Search OpenSubtitles for matching subtitles'''
         search = []
         for movie in self.moviefiles:
             search.append({'sublanguageid': self.lang_id,
-                           'moviehash': movie['hash'], 
+                           'moviehash': movie['hash'],
                            'moviebytesize': str(movie['size'])})
 
         # fixes weird problem where subs aren't found when only searching
@@ -84,20 +86,20 @@ class SubtitleDownload:
         if resp['data'] == False:
             print("No subtitles found")
             return
-        
+
         subtitles = []
         for result in resp['data']:
             if int(result['SubBad']) != 1:
                 subtitles.append({'subid': result['IDSubtitleFile'],
                                   'hash': result['MovieHash']})
-        
+
         downloadable_subs = subtitles[:]
         hash = None
         for s in subtitles:
             if hash == s['hash']:
                 downloadable_subs.remove(s)
             hash = s['hash']
-            
+
         for ds in downloadable_subs:
             sub = self.download_subtitles(ds)
             for movie in self.moviefiles:
@@ -107,7 +109,7 @@ class SubtitleDownload:
                     file = open(filename, "wb")
                     file.write(sub)
                     file.close()
-    
+
     def download_subtitles(self, subtitle):
         resp = self.server.DownloadSubtitles(self.login_token, [subtitle['subid']])
         self.check_status(resp)
@@ -127,7 +129,7 @@ class SubtitleDownload:
 
     def check_status(self, resp):
         '''Check the return status of the request.
-        
+
         Anything other than "200 OK" raises a UserWarning
         '''
         if resp['status'].upper() != '200 OK':
@@ -135,44 +137,43 @@ class SubtitleDownload:
 
     def hashFile(self, name):
         '''Calculates the hash value of a movie.
-        
-        Copied from OpenSubtitles own examples: 
+
+        Copied from OpenSubtitles own examples:
             http://trac.opensubtitles.org/projects/opensubtitles/wiki/HashSourceCodes
         '''
-        try: 
-            longlongformat = 'q'  # long long 
-            bytesize = struct.calcsize(longlongformat) 
-            
+        try:
+            longlongformat = 'q'  # long long
+            bytesize = struct.calcsize(longlongformat)
+
             f = open(name, "rb")
-            
+
             filesize = os.path.getsize(name)
-            hash = filesize 
-            
-            if filesize < 65536 * 2: 
-                return "SizeError" 
-            
-            for x in range(65536//bytesize): 
-                buffer = f.read(bytesize) 
-                (l_value,)= struct.unpack(longlongformat, buffer)  
-                hash += l_value 
-                hash = hash & 0xFFFFFFFFFFFFFFFF #to remain as 64bit number  
-            
-            f.seek(max(0,filesize-65536),0) 
-            for x in range(65536//bytesize): 
-                buffer = f.read(bytesize) 
-                (l_value,)= struct.unpack(longlongformat, buffer)  
-                hash += l_value 
-                hash = hash & 0xFFFFFFFFFFFFFFFF 
-            
-            f.close() 
-            returnedhash =  "%016x" % hash 
-            return returnedhash 
-        except(IOError): 
+            hash = filesize
+
+            if filesize < 65536 * 2:
+                return "SizeError"
+
+            for x in range(65536//bytesize):
+                buffer = f.read(bytesize)
+                (l_value,)= struct.unpack(longlongformat, buffer)
+                hash += l_value
+                hash = hash & 0xFFFFFFFFFFFFFFFF #to remain as 64bit number
+
+            f.seek(max(0,filesize-65536),0)
+            for x in range(65536//bytesize):
+                buffer = f.read(bytesize)
+                (l_value,)= struct.unpack(longlongformat, buffer)
+                hash += l_value
+                hash = hash & 0xFFFFFFFFFFFFFFFF
+
+            f.close()
+            returnedhash =  "%016x" % hash
+            return returnedhash
+        except(IOError):
             return "IOError"
 
 
 if __name__ == '__main__':
-    import os
     cwd = os.getcwd()
     if len(sys.argv) > 1:
         cwd = sys.argv[1]
